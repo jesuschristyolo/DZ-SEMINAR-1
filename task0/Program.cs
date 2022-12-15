caeeb3bc5d76d7596ce4e0609742edd3533f78ce
Rustam <valishev57@mail.ru>
2022-12-15 22:29:46 +0500

    квадрат

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,2 +1,8 @@
 ﻿
-Console.WriteLine("Hello, World!");
+using static System.Console;
+
+WriteLine("Введите число: ");
+
+int a =int.Parse (ReadLine()!);
+// int b = Convert.ToInt32 (ReadLine());
+WriteLine($"Квадрат числа {a} равен {a*a}");
